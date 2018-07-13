d1c42adea21ef85d9af42a02df7a6bc73913a2bd
Funmi <folowookere@yahoo.com>
2018-07-12 23:21:54 -0800

    Falsey Bouncer

---
+++ falsybouncer.js
@@ -0,0 +1,13 @@
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  newArray = arr.filter(function(myArgument) {
+
+
+  	return (Boolean(myArgument) === true);
+  })
+  return newArray;
+}
+
+console.log(
+bouncer([7, "ate", "", false, 9])
+);
