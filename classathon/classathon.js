dcb810a2e3000457ace8f1f0cc018055c21da244
Funmi <folowookere@yahoo.com>
2018-07-24 15:57:54 -0700

    Classathon Wk 7/22

---
+++ classathon/classathon.js
@@ -0,0 +1,11 @@
+//Libraries that need to be included
+
+#include <cs50.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+//the main function will take 2 arguments
+int main(int argc, string argv[])
+{
---
+++ classathon/classathon3.c
@@ -0,0 +1,27 @@
+//Libraries that need to be included
+
+#include <cs50.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+//the main function will take 2 arguments
+int main(int argc, string argv[])
+{
+
+    int i = atoi(argv[1]);
+    int a = 0;
+    int b = 0;
+
+     if (argc != 2)
+    {
+
+        printf("Program cannot continue. \n");
+        return 1;
+    }
+
+   if (i % 2 != 0)
+   printf("")
+
+}
