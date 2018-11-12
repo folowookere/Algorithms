2e10b265b6d32bfe1b9247a475d246a710ce00c0
Funmi <folowookere@yahoo.com>
2018-11-12 17:30:03 -0500

    Basic do while loop

---
+++ dowhile_loop.js
@@ -0,0 +1,11 @@
+// The loop will push the number 10 to myArray, and i will be equal to 11 when te code finishes running
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i++;
+} while (i < 10);
