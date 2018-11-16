55275ea16515512baa56c1196f3b49faf3e12aa7
Funmi <folowookere@yahoo.com>
2018-11-16 16:33:23 -0500

    Basic use of ternary operator

---
+++ ternaryoperator_example.js
@@ -0,0 +1,11 @@
+// Conditional (ternary) operator example
+
+function checkEqual(a, b) {
+
+/* Check if a and b are equal, 
+ if they are return true otherwise return false */
+return a == b ? true : false;
+
+}
+
+checkEqual(1, 2);
