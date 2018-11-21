7fc7c791df5299675dc79fefe47fbb7d95f6da63
Funmi <folowookere@yahoo.com>
2018-11-20 20:07:20 -0500

    Updated Manipulating Complex Objects file

--- recordcollection.js
+++ recordcollection.js
@@ -28,26 +28,18 @@ var collection = {
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
-
-/* 
-    First checks if prop is equal to tracks AND if value isn’t a blank string. 
-    If both tests pass, value is pushed into the tracks array.
-    If that first check doesn’t pass, it next checks only if value isn’t a blank string. 
-    If that test passes, either a new key (prop) and value (value) are added to the object, 
-    or an existing key is updated if the prop already exists.
-    If both these checks fail (meaning value must be an empty string), then the key (prop) is removed from the object.
-*/
 function updateRecords(id, prop, value) {
-  if (prop === "tracks" && value !== "") {
+  if(prop === "tracks" && value !== "") {
     if(collection[id][prop]) {
       collection[id][prop].push(value);
     }
-    else {
+    else  {
       collection[id][prop] = [value];
     }
-}
-  else if (value !== "") {
-    delete collection[id][prop];
+  } else if (value !== "") {
+    collection[id][prop] = value;
+} else {
+  delete collection[id][prop];
 }
   return collection;
 }
