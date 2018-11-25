1b00aa649cf20bbe5a3833f98a8694cc031884ff
Funmi <folowookere@yahoo.com>
2018-11-24 22:04:43 -0800

    Updated comments to describe file correctly

--- regex_testmethod.js
+++ regex_testmethod.js
@@ -1,5 +1,6 @@
-/* . The .test() method takes the regex, applies it to a string (which is placed inside the parentheses),
-and returns true or false if your pattern finds something or not. */
+/* . The .test() method takes the regex, applies it to a string
+(which is placed inside the parentheses), and returns true or false if 
+your pattern finds something or not. */
 
 let myString = "Hello, World!";
 let myRegex = /Hello/;
