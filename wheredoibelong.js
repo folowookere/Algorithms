674eb4963c4d2d046405f2d73a5280c8bb1fed2c
Funmi <folowookere@yahoo.com>
2018-12-01 00:10:23 -0500

    Final file for returning index

--- wheredoibelong.js
+++ wheredoibelong.js
@@ -1,11 +1,13 @@
-//Return index of num 
+// Return index of num variable
 
 function getIndexToIns(arr, num) {
 
   // Insert num into arr array
   arr.push(num);
+  
   // Sort array into ascending order
   arr.sort(function(a, b){return a - b});
+  
   // Find my place in this sorted array.
   return arr.indexOf(num);
 }
